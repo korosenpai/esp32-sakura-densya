65a2c96930bda6f4f67768e9af5bc4412833b4bf
koro <koro.dev404@gmail.com>
2023-09-03 19:41:50 +0200

    main file while handle everything

--- boot.py
+++ boot.py
@@ -1,1 +1,6 @@
+#import test_components
+#import tests.webServer.getData
+
+#from utils.web_server import web_server; web_server()
+
 
---
+++ main.py
@@ -0,0 +1,10 @@
+from utils.leds import Led
+
+print("preparing leds...")
+shop_led = Led(33)
+ceiling_led = Led(26)
+
+# in  a new thread
+print("connecting to wifi...")
+
+
