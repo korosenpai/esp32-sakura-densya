bdda44b0f317eb85a553705a9c8cf4d185be9812
koro <koro.dev404@gmail.com>
2023-09-16 18:21:15 +0200

    moved led api in led.py file

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import _thread
+from time import sleep
 
-from utils.led import Led
+from utils.led import ceiling_led, shop_led
 from utils.connect_to_wifi import connect_to_wifi
 from utils.web_server import web_server
 
-print("preparing leds...")
-shop_led = Led(33)
-ceiling_led = Led(26)
+print("testing leds...")
+ceiling_led.set(100)
+shop_led.set(100)
+sleep(2)
+ceiling_led.set(0)
+shop_led.set(0)
 
-# calculate time and sleep on other thread
 
 
 print("connecting to wifi...")
 station = connect_to_wifi()
-web_server(station, ceiling_led, shop_led)
+web_server(station)
+
